84509db52cc7924a84bd936c28312d7304795ec5
Margarita Vasileva <m.h.vasileva@gmail.com>
2016-09-05 14:52:27 +0300

    finish initial implementation

--- app.js
+++ app.js
@@ -6,10 +6,39 @@ var io = require('socket.io')(server);
 server.listen(3000);
 
 app.use(express.static(__dirname));
+app.use(express.static('node_modules'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
+var players = [];
+var letters = ['X', 'O'];
+
+io.sockets.on('connection', function (socket) {
+  var player = {
+    id: socket.id
+  };
+  players.push(player);
+
+  socket.emit('setPlayer', { letter: letters[players.length % 2] });
+
+  if (players.length == 2) {
+    socket.emit('roomReady', { onTurn: "X" });
+  } else {
+    console.log(players.length);
+  }
+
+  socket.on('makeMove', function (data) {
+    var otherPlayer = "X";
+    if (data.player == "X") {
+      otherPlayer = "O";
+    }
+
+    socket.broadcast.emit('finishMove', { player: data.player, position: data.position, onTurn: otherPlayer });
+  });
+
+  socket.on('newGame', function (data) {
+    socket.broadcast.emit('launchNewGame', { onTurn: data.player });
+  });
 
 });
--- test.js
+++ test.js
@@ -1,4 +1,80 @@
 (function () {
-  var socket = io.connect('http://localhost');
-  console.log(socket);
+  var socket = io.connect('http://localhost:3000');
+  var symbol = "";
+  var onTurn = false;
+
+  var isAvailable = function (cellId) {
+    var elem = document.getElementById(cellId);
+    if (elem.innerText != "") {
+      return false;
+    }
+    return true;
+  };
+
+  var newGame = function (winner) {
+    for (i = 1; i <= 9; i++) {
+      document.getElementById(i).innerText = "";
+    }
+    onTurn = symbol !== winner;
+  };
+
+  socket.on('launchNewGame', function (data) {
+    newGame(data.onTurn);
+  });
+
+  socket.on('setPlayer', function (data) {
+    console.log("player set: ", data.letter);
+    symbol = data.letter;
+    document.getElementById("player").innerText += symbol;
+  });
+
+  socket.on('roomReady', function (data) {
+    onTurn = data.onTurn === symbol;
+  });
+
+  socket.on('finishMove', function (data) {
+    document.getElementById(data.position).innerText = data.player;
+    onTurn = data.onTurn === symbol;
+  });
+
+  var allLike = function (list, symbol) {
+    var symbols = [];
+    list.forEach(function (s) {
+      var currentSymbol = document.getElementById(s).innerHTML;
+      if (currentSymbol !== "" && currentSymbol === symbol) {
+        symbols.push(currentSymbol);
+      }
+    });
+    if (symbols.length === 3) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
+  var isWinner = function (playerSymbol) {
+    var winningRows = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+      [1, 4, 7], [2, 5, 8], [3, 6, 9],
+      [1, 5, 9], [3, 5, 7]];
+    winningRows.forEach(function (row) {
+      var winner = allLike(row, playerSymbol);
+      if (winner) {
+        alert("winner is:" + playerSymbol);
+        socket.emit("newGame", { player: playerSymbol });
+        newGame(playerSymbol);
+      };
+    });
+  };
+
+  $('td').click(function (eventData) {
+    if (onTurn && isAvailable(eventData.target.id)) {
+      socket.emit('makeMove', { player: symbol, position: eventData.target.id });
+      document.getElementById(eventData.target.id).innerText = symbol;
+      isWinner(symbol);
+      onTurn = false;
+    }
+    return false;
+  });
+
 })();
